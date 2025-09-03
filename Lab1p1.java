35f6d691e148bc1209a8ba9fb21523e4e2e93112
24AG1A05I6 <thumkuntashravankumar@gmail.com>
2025-09-03 11:39:10 +0530

    Add files via upload

---
+++ Lab1p1.java
@@ -0,0 +1,37 @@
+class Lab1p1{
+	public static void main(String...arys){
+		//byte b = 15;
+		//Byte bb = b; //AutoBoxing
+		
+		//int x = 200;
+		//byte b = (byte)x;
+		
+		//System.out.println(Byte.MAX_VALUE);
+		//System.out.println(bb.MIN_VALUE);
+		//System.out.println(Byte.BYTES);
+		//System.out.println(bb.SIZE);
+		//System.out.println(Byte.TYPE);
+		
+		//short s=15;
+		//Short ss=s;
+		
+		//System.out.println(Short.MAX_VALUE);
+		//System.out.println(ss.MIN_VALUE);
+		//System.out.println(ss.BYTES);
+		//System.out.println(ss.SIZE);
+		//System.out.println(ss.TYPE);
+		
+		int i=15;
+		Integer ii=i;
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(ii.MIN_VALUE);
+		System.out.println(ii.BYTES);
+		System.out.println(ii.SIZE);
+		System.out.println(ii.TYPE);
+			
+		
+			
+			
+	}
+}
