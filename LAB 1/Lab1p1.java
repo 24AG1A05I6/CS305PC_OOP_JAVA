1ad48a8b5463e4aeaaf84c528403ea3a32687c82
24AG1A05I6 <thumkuntashravankumar@gmail.com>
2025-09-04 10:25:46 +0530

    Update Lab1p1.java

--- LAB 1/Lab1p1.java
+++ LAB 1/Lab1p1.java
@@ -1,25 +1,25 @@
 class Lab1p1{
 	public static void main(String...arys){
-		//byte b = 15;
-		//Byte bb = b; //AutoBoxing
+		byte b = 15;
+		Byte bb = b; //AutoBoxing
 		
 		//int x = 200;
 		//byte b = (byte)x;
 		
-		//System.out.println(Byte.MAX_VALUE);
-		//System.out.println(bb.MIN_VALUE);
-		//System.out.println(Byte.BYTES);
-		//System.out.println(bb.SIZE);
-		//System.out.println(Byte.TYPE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(Byte.BYTES);
+		System.out.println(bb.SIZE);
+		System.out.println(Byte.TYPE);
 		
-		//short s=15;
-		//Short ss=s;
+		short s=15;
+		Short ss=s;
 		
-		//System.out.println(Short.MAX_VALUE);
-		//System.out.println(ss.MIN_VALUE);
-		//System.out.println(ss.BYTES);
-		//System.out.println(ss.SIZE);
-		//System.out.println(ss.TYPE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.BYTES);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.TYPE);
 		
 		int i=15;
 		Integer ii=i;
@@ -29,9 +29,41 @@ class Lab1p1{
 		System.out.println(ii.BYTES);
 		System.out.println(ii.SIZE);
 		System.out.println(ii.TYPE);
-			
 		
-			
-			
+		long l=15;
+		Long ll=i;
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(ll.MIN_VALUE);
+		System.out.println(ll.BYTES);
+		System.out.println(ll.SIZE);
+		System.out.println(ll.TYPE);
+		
+		float f=15;
+		Float ff=f;
+
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(ff.MIN_VALUE);
+		System.out.println(ff.BYTES);
+		System.out.println(ff.SIZE);
+		System.out.println(ff.TYPE);
+
+		double d=15;
+	    Double dd=d;
+		
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(dd.MIN_VALUE);
+		System.out.println(dd.BYTES);
+		System.out.println(dd.SIZE);
+		System.out.println(dd.TYPE);
+		
+		boolean z=true;
+	    Boolean z=z;
+
+		System.out.println(Boolean.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(bb.BYTES);
+		System.out.println(bb.SIZE);
+		System.out.println(bb.TYPE);
 	}
 }
